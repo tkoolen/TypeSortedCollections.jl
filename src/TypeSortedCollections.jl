dfc4341d6ce04f4d8536470b92cfb40c2590524d
Twan Koolen <koolen.twan@gmail.com>
2017-08-09 21:41:47 -0400

    Copy code over from RigidBodyDynamics.jl.

--- src/TypeSortedCollections.jl
+++ src/TypeSortedCollections.jl
@@ -1,5 +1,79 @@
 module TypeSortedCollections
 
-# package code goes here
+export
+    TypeSortedCollection,
+    indexfun # TODO: remove
+
+const TupleOfVectors = Tuple{Vararg{Vector{T} where T}}
+
+struct TypeSortedCollection{D<:TupleOfVectors, I}
+    data::D
+    indexfun::I
+
+    function TypeSortedCollection(A, indexfun::I) where {I}
+        types = unique(typeof.(A))
+        D = Tuple{[Vector{T} for T in types]...}
+        TypeSortedCollection{D, I}(A, indexfun)
+    end
+
+    function TypeSortedCollection{D}(indexfun::I) where {D<:TupleOfVectors, I}
+        eltypes = eltype.([D.parameters...])
+        data = tuple((T[] for T in eltypes)...)
+        new{D, I}(data, indexfun)
+    end
+
+    function TypeSortedCollection{D, I}(A, indexfun::I) where {D<:TupleOfVectors, I}
+        append!(TypeSortedCollection{D}(indexfun), A)
+    end
+end
+
+function Base.append!(dest::TypeSortedCollection, A)
+    data = dest.data
+    type_to_index = Dict(T => i for (i, T) in enumerate(eltype.(data)))
+    for x in A
+        T = typeof(x)
+        push!(data[type_to_index[T]], x)
+    end
+    dest
+end
+
+Base.length(x::TypeSortedCollection) = sum(length, x.data)
+indexfun(x::TypeSortedCollection) = x.indexfun
+
+@generated function Base.map!(f, dest::AbstractVector, tsc::TypeSortedCollection{D}, As::AbstractVector...) where {D}
+    expr = Expr(:block)
+    push!(expr.args, :(Base.@_inline_meta))
+    for i = 1 : nfields(D)
+        push!(expr.args, quote
+            let vec = tsc.data[$i]
+                for j in eachindex(vec)
+                    element = vec[j]
+                    index = tsc.indexfun(element)
+                    dest[index] = f(element, getindex.(As, index)...)
+                end
+            end
+        end)
+    end
+    push!(expr.args, :(return nothing))
+    expr
+end
+
+@generated function Base.foreach(f, tsc::TypeSortedCollection{D}, As::AbstractVector...) where {D}
+    expr = Expr(:block)
+    push!(expr.args, :(Base.@_inline_meta))
+    for i = 1 : nfields(D)
+        push!(expr.args, quote
+            let vec = tsc.data[$i]
+                for j in eachindex(vec)
+                    element = vec[j]
+                    index = tsc.indexfun(element)
+                    f(element, getindex.(As, index)...)
+                end
+            end
+        end)
+    end
+    push!(expr.args, :(return nothing))
+    expr
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,30 @@
 using TypeSortedCollections
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+x = Pair[3. => 1; 4 => 2; 5 => 3]
+sortedx = TypeSortedCollection(x, last)
+index = indexfun(sortedx)
+@test index == last
+@test length(sortedx) == length(x)
+
+f(x::Pair{Int64, Int64}) = 3 * first(x)
+f(x::Pair{Float64, Int64}) = round(Int64, first(x) / 2)
+
+maptest(f, results, index, x) = begin
+    for element in x
+        results[index(element)] != f(element) && return false
+    end
+    true
+end
+
+results = Vector{Int64}(maximum(index.(x)))
+map!(f, results, sortedx)
+@test maptest(f, results, index, x)
+
+y = Pair[1. => 4; 2. => 2; 3. => 3]
+sortedy = typeof(sortedx)(y, index)
+@test typeof(sortedy) == typeof(sortedx)
+
+results = Vector{Int64}(maximum(index.(y)))
+map!(f, results, sortedy)
+@test maptest(f, results, index, y)
