72c945ee6b5a3b96d428209d391fa00845ea103a
Twan Koolen <koolen.twan@gmail.com>
2017-09-23 13:18:12 -0400

    Simplify.

--- test_order_bug.jl
+++ test_order_bug.jl
@@ -2,11 +2,8 @@ using Base.Test
 using TypeSortedCollections
 
 module M
-g(x::Int64, y1::Float64, y2::Int64) = x * y1 * y2
-g(x::Float64, y1::Float64, y2::Int64) = x + y1 + y2
 g(x::Int64, y1::Float64, y2::Float64) = x * y1 - y2
 g(x::Float64, y1::Float64, y2::Float64) = x + y1 - y2
-g(x::Int64, y1::Int64, y2::Float64) = x - y1 * y2
 end
 
 @testset "broadcast! length mismatch" begin
