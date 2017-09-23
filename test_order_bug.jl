c75d9cca1d8164f1494bb647350f95e6ddd573cd
Twan Koolen <koolen.twan@gmail.com>
2017-09-23 14:00:25 -0400

    Simplify

--- test_order_bug.jl
+++ test_order_bug.jl
@@ -1,28 +1,52 @@
-using Base.Test
-using TypeSortedCollections
+module A
 
-module M
-g(x::Int64, y1::Float64, y2::Float64) = x * y1 - y2
-g(x::Float64, y1::Float64, y2::Float64) = x + y1 - y2
+export
+    TypeSortedCollection
+
+const TupleOfVectors = Tuple{Vararg{Vector{T} where T}}
+
+struct TypeSortedCollection{D<:TupleOfVectors, N}
+    TypeSortedCollection{D}() where {D} = new{D, length(D.parameters)}()
 end
 
+function TypeSortedCollection(A)
+   types = unique(typeof.(A))
+   D = Tuple{[Vector{T} for T in types]...}
+   TypeSortedCollection{D}()
+end
+
+Base.Broadcast._containertype(::Type{<:TypeSortedCollection}) = TypeSortedCollection
+Base.Broadcast.promote_containertype(::Type{TypeSortedCollection}, _) = TypeSortedCollection
+Base.Broadcast.promote_containertype(_, ::Type{TypeSortedCollection}) = TypeSortedCollection
+Base.Broadcast.promote_containertype(::Type{TypeSortedCollection}, ::Type{Array}) = TypeSortedCollection
+Base.Broadcast.promote_containertype(::Type{Array}, ::Type{TypeSortedCollection}) = TypeSortedCollection
+
+function Base.Broadcast.broadcast_c!(f, ::Type, ::Type{TypeSortedCollection}, dest::AbstractVector, A, Bs...)
+    throw(DimensionMismatch())
+end
+
+end # module
+
+function g end
+
+using Base.Test
 @testset "broadcast! length mismatch" begin
     x = Number[3.; 4; 5]
-    sortedx = TypeSortedCollection(x)
+    sortedx = A.TypeSortedCollection(x)
     results = rand(length(x) + 1)
     y1 = rand()
     y2 = rand(Int)
-    @test_throws DimensionMismatch results .= M.g.(sortedx, y1, y2)
+    @test_throws DimensionMismatch results .= g.(sortedx, y1, y2)
 end
 
 @testset "broadcast! matching indices" begin
     x = Number[3.; 4; 5]
-    sortedx = TypeSortedCollection(x)
+    sortedx = A.TypeSortedCollection(x)
     y1 = rand()
     y2 = [7.; 8.; 9.]
-    sortedy2 = TypeSortedCollection(y2)
+    sortedy2 = A.TypeSortedCollection(y2)
     results = similar(x, Float64)
-    broadcast!(M.g, results, sortedx, y1, sortedy2)
-    @allocated broadcast!(M.g, results, sortedx, y1, sortedy2)
+    broadcast!(g, results, sortedx, y1, sortedy2)
+    @allocated broadcast!(g, results, sortedx, y1, sortedy2)
 end
 
